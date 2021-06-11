4dcf90ca7f3f6d48e4ca57c7a14f0dd139824cdc
Angga Bawole <bawoleangga03@gmail.com>
2021-06-11 14:31:56 +0800

    initial commit

---
+++ blade/pages/home-page.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.home')
+@section('title', 'Document')
+@section('content')
+
+
+
+@endsection
+@push('addon-script')
+  <script>
+    
+  </script>
+@endpush
---
+++ blade/pages/page.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.home')
+@section('title')
+  Document
+@endsection
+@section('content')
+
+
+
+@endsection
+@push('addon-script')
+  <script>
+    
+  </script>
+@endpush
